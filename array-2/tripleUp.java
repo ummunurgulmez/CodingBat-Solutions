38c9a0292f26a64286ddf52230dff1760a8fc394
Ümmü Nur Gülmez <68664491+ummunurgulmez@users.noreply.github.com>
2023-04-19 17:39:03 +0300

    Create tripleUp.java

---
+++ array-2/tripleUp.java
@@ -0,0 +1,8 @@
+public boolean tripleUp(int[] nums) {
+  for(int i=0; i<=nums.length-3; i++){
+    if(nums[i+1]==nums[i]+1 && nums[i+2]== nums[i+1]+1){
+      return true;
+    }
+  }
+    return false;
+}
