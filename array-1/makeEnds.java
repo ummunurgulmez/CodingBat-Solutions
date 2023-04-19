eba94410d01e7af785bfef48e565c2548f3ffdf1
Ümmü Nur Gülmez <68664491+ummunurgulmez@users.noreply.github.com>
2023-04-19 17:21:51 +0300

    Create makeEnds.java

---
+++ array-1/makeEnds.java
@@ -0,0 +1,12 @@
+public int[] makeEnds(int[] nums) {
+  int[] arr = new int [2];
+  if(nums.length != 1){
+    arr[0] = nums[0];
+    arr[1] = nums[nums.length-1];
+  }
+  else{
+  arr[0]=nums[0];
+  arr[1]=nums[0];
+} 
+return arr;
+}
