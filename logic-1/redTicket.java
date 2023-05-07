bb9faaad5f0ca207a912f371eb8dbe065a77567d
Ümmü Nur Gülmez <68664491+ummunurgulmez@users.noreply.github.com>
2023-05-07 15:08:10 +0300

    Create redTicket.java

---
+++ logic-1/redTicket.java
@@ -0,0 +1,13 @@
+public int redTicket(int a, int b, int c) {
+ 
+  if(a ==2 &&b ==2 && c ==2){
+    return 10;
+  }
+  if(a==b && b==c && a!=2){
+    return 5;
+  }
+  if(a !=b && a !=c){
+    return 1;
+  }
+  return 0;
+}
