e93c20373e047c267fd7d27e22b139df582cc7bc
Ümmü Nur Gülmez <68664491+ummunurgulmez@users.noreply.github.com>
2023-05-07 18:33:18 +0300

    Last solution of Logic-1

---
+++ logic-1/alarmClock.java
@@ -0,0 +1,18 @@
+public String alarmClock(int day, boolean vacation) {
+  if (vacation) {
+    if (day >= 1 && day <= 5) {
+      return "10:00";
+    } 
+    else { 
+      return "off";
+    }
+  } 
+  else { 
+    if (day >= 1 && day <= 5) { 
+      return "7:00";
+    } 
+    else { 
+      return "10:00";
+    }
+  }
+}
